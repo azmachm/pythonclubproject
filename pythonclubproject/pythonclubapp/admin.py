8cb14d5cbf46582fe09110efa5599252dbe65fd2
azmachm <azmachm@gmail.com>
2020-02-19 14:15:35 -0800

    home work

--- pythonclubproject/pythonclubapp/admin.py
+++ pythonclubproject/pythonclubapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import ProductType, Product, Review
+from .models import TechType, Product, Review
 
 # Register your models here.
-admin.site.register(ProductType)
+admin.site.register(TechType)
 admin.site.register(Product)
 admin.site.register(Review)
--- pythonclubproject/pythonclubapp/urls.py
+++ pythonclubproject/pythonclubapp/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
 from . import views
+from django.urls import path
 
-urlpatterns = [
-    path ('', views.index, name='index'),
-    
+# this is acomment
+urlpatterns=[
+    path('', views.index, name='index'),
 ]
+
